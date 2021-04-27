ea24e6a2a30160f3825f7f8d05c73bc9c3ac7667
My Tran <myytran28@gmail.com>
2021-04-27 19:14:00 -0400

    add content to views

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-const mongoose = require('mongoose');
-const passport = require('passport');
+const mongoose = require("mongoose");
+const passport = require("passport");
 
+//set up our express application
+app.use(express.static("public"));
 
-
-
-app.use(express.static('public'));
-
+//register view engine
+app.set("view engine", "ejs");
 
 //launch
 app.listen(port);
-console.log('The magic happens on port ' + port);
-
+console.log("The magic happens on port " + port);
