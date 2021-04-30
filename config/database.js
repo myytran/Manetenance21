bce04e147de0a3ae1271eb3c5990d0bbdc82e7bf
My Tran <myytran28@gmail.com>
2021-04-29 20:49:38 -0400

    add routes and ejs files

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 //this file will hold our database connection settings
 module.exports = {
-
-  
+  MongoURI:
+    'mongodb+srv://maneUser:1234@cluster0.waflr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 };
--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
-const mongoose = require("mongoose");
-const passport = require("passport");
+const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const passport = require('passport');
+const expressLayouts = require('express-ejs-layouts');
+const ejs = require('ejs');
 
-//set up our express application
-app.use(express.static("public"));
+//DB config
+const db = require('./config/database').MongoURI;
+//CONNECT TO mongo
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('mongodb connected..'))
+  .catch((err) => console.log(err));
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
-//register view engine
-app.set("view engine", "ejs");
+//Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
 
 //launch
 app.listen(port);
-console.log("The magic happens on port " + port);
+console.log('The magic happens on port ' + port);
